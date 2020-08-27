4bda9960e7ffb2abe46c8c6b407f73d26abf40b1
Sumit Makkar <makkarsumit1986@gmail.com>
2020-08-27 14:30:11 +0530

    View Model file added

---
+++ NetmedsAssignment/View Model/TestPackageViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TestPackageViewModel.swift
+//  NetmedsAssignment
+//
+//  Created by Sumit Makkar on 27/08/20.
+//  Copyright © 2020 Sumit Makkar. All rights reserved.
+//
+
+import Foundation
