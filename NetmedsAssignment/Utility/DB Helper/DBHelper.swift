c6dd48347f7048748a312f4e6625f855a5b97da1
Sumit Makkar <makkarsumit1986@gmail.com>
2020-08-28 22:19:45 +0530

    DB Helper added

---
+++ NetmedsAssignment/Utility/DB Helper/DBHelper.swift
@@ -0,0 +1,166 @@
+//
+//  DBHelper.swift
+//  NetmedsAssignment
+//
+//  Created by Sumit Makkar on 28/08/20.
+//  Copyright © 2020 Sumit Makkar. All rights reserved.
+//
+
+import Foundation
+import SQLite3
+
+enum DatabaseCRUDOperation: String
+{
+    case insertion = "INSERT INTO "
+    case read      = "SELECT * FROM "
+}
+
+class DBHelper
+{
+    var netmedsDatabase: OpaquePointer?
+    var databaseName   : String = "netmedsAppDatabase.sqlite"
+    
+    init(with tableQuery: String? = nil)
+    {
+        netmedsDatabase = createDB()
+        if let query = tableQuery
+        {
+            createTable(with: query)
+        }
+    }
+    
+    private func createDB() -> OpaquePointer?
+    {
+        guard let filePath = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathExtension(databaseName) else { return nil }
+        debugPrint("DB File created at: \(filePath)")
+        
+        var database: OpaquePointer? = nil
+        
+        if sqlite3_open(filePath.path, &database) != SQLITE_OK
+        {
+            debugPrint("There is error in creating DB")
+            return nil
+        }
+        else
+        {
+            debugPrint("Database has been created with path \(databaseName)")
+            return database
+        }
+    }
+    
+    func createTable(with query: String)
+    {
+        var statement: OpaquePointer? = nil
+        
+        if sqlite3_prepare_v2(netmedsDatabase, query, -1, &statement, nil) == SQLITE_OK
+        {
+            if sqlite3_step(statement) == SQLITE_DONE
+            {
+                debugPrint("Table creation success")
+            }
+            else
+            {
+                debugPrint("Table creation fail")
+            }
+        }
+        else
+        {
+            debugPrint("Prepration fail")
+        }
+    }
+    
+    func insertIntoTable(tableName: String, fromDictionary attributesAndValuesDictionary: [String: Any]?)
+    {
+        guard let attrValDict = attributesAndValuesDictionary else { return }
+        let query = createQuery(for: DatabaseCRUDOperation.insertion, with: attrValDict, in: tableName)
+        
+        var statement: OpaquePointer? = nil
+        
+        if sqlite3_prepare_v2(netmedsDatabase, query, -1, &statement, nil) == SQLITE_OK
+        {
+            var index: Int32 = 0
+            for dictKey in attrValDict.keys
+            {
+                index += 1
+                if let value = attrValDict[dictKey] as? Int
+                {
+                    sqlite3_bind_int(statement, index, Int32(value))
+                }
+                else if let value = attrValDict[dictKey] as? NSString
+                {
+                    sqlite3_bind_text(statement, index, value.utf8String, -1, nil)
+                }
+            }
+            
+            if sqlite3_step(statement) == SQLITE_DONE
+            {
+                debugPrint("Data inserted success")
+            }
+            else
+            {
+                debugPrint("Data is not inserted in table")
+            }
+        }
+        else
+        {
+            debugPrint("Query is not as per requirement")
+        }
+        
+    }
+    
+    func readTestPackagesFromTable(tableName: String, where whereQueryClause: String? = nil) -> TestPackagesModel
+    {
+        let query = DatabaseCRUDOperation.read.rawValue + tableName + (whereQueryClause != nil ? " WHERE \(whereQueryClause!);" : ";")
+        var statement: OpaquePointer? = nil
+        var testPackagesModel         = TestPackagesModel()
+        if sqlite3_prepare_v2(netmedsDatabase, query, -1, &statement, nil) == SQLITE_OK
+        {
+            while sqlite3_step(statement) == SQLITE_ROW
+            {
+                let sNo           = Int(sqlite3_column_int(statement, 0))
+                let itemID        = String(cString: sqlite3_column_text(statement, 1))
+                let itemName      = String(cString: sqlite3_column_text(statement, 2))
+                let type          = String(cString: sqlite3_column_text(statement, 3))
+                let keyword       = String(cString: sqlite3_column_text(statement, 4))
+                let bestSellers   = String(cString: sqlite3_column_text(statement, 5))
+                let testCount     = Int(sqlite3_column_int(statement, 6))
+                let includedTests = String(cString: sqlite3_column_text(statement, 7))
+                let url           = String(cString: sqlite3_column_text(statement, 8))
+                let minPrice      = Int(sqlite3_column_int(statement, 9))
+                let labName       = String(cString: sqlite3_column_text(statement, 10))
+                let fasting       = Int(sqlite3_column_int(statement, 11))
+                let availableAt   = Int(sqlite3_column_int(statement, 12))
+                let popular       = String(cString: sqlite3_column_text(statement, 13))
+                let category      = String(cString: sqlite3_column_text(statement, 14))
+                let objectID      = String(cString: sqlite3_column_text(statement, 15))
+
+                let testPackageElement = TestPackagesModelElement(sNo: sNo, itemID: itemID, itemName: itemName, type: type, keyword: keyword, bestSellers: bestSellers, testCount: testCount, includedTests: includedTests, url: url, minPrice: minPrice, labName: labName, fasting: fasting, availableAt: availableAt, popular: popular, category: category, objectID: objectID)
+                testPackagesModel.append(testPackageElement)
+            }
+        }
+        return testPackagesModel
+    }
+        
+    // MARK: - Prvate Methods
+    private func createQuery(for operation: DatabaseCRUDOperation, with attributesAndValuesDictionary: [String: Any], in tableName: String) -> String
+    {
+        var attributesString = " ("
+        var valuesString     = " ("
+        for attributeKey in attributesAndValuesDictionary.keys
+        {
+            if attributesString != " ("
+            {
+                attributesString += ", \(attributeKey)"
+                valuesString     += ", ?"
+            }
+            else
+            {
+                attributesString += attributeKey
+                valuesString     += "?"
+            }
+        }
+        attributesString += ")"
+        valuesString     += ");"
+        return (operation.rawValue + tableName + attributesString + " VALUES " + valuesString)
+    }
+}
--- NetmedsAssignment/View Model/TestPackageViewModel.swift
+++ NetmedsAssignment/View Model/TestPackageViewModel.swift
@@ -12,11 +12,26 @@ class TestPackageViewModel
 {
     private var testPackagesDataModel        : TestPackagesModel?
     private var filteredTestPackagesDataModel: TestPackagesModel?
+    private let testPackagesTableName        : String = "testPackages"
+    private var databaseHelper               : DBHelper?
+    private var cartArray                    : TestPackagesModel?
+    
+    private var testPackagesTableCreationQuery: String
+    {
+        return "CREATE TABLE IF NOT EXISTS \(testPackagesTableName) (sNo INTEGER PRIMARY KEY, itemID TEXT, itemName TEXT, type TEXT, keyword TEXT, bestSellers TEXT, testCount INTEGER, includedTests TEXT, url TEXT, minPrice INTEGER, labName TEXT, fasting INTEGER, availableAt INTEGER, popular TEXT, category TEXT, objectID TEXT);"
+    }
     
     var numberOfRows: Int
     {
         guard let dataModel = filteredTestPackagesDataModel else { return 0 }
         return dataModel.count
+    }
+    
+    // MARK: - Initializer
+    init()
+    {
+        databaseHelper = DBHelper(with: testPackagesTableCreationQuery)
+        databaseHelper?.readTestPackagesFromTable(tableName: testPackagesTableName)
     }
     
     //updateActualIndexesOfTestPackageData() will redude the complexity of finding element in testPackagesDataModel to O(1)
@@ -34,6 +49,102 @@ class TestPackageViewModel
     {
         guard let dataModel = filteredTestPackagesDataModel, indexPath.row < dataModel.count else { return false }
         return dataModel[indexPath.row].isAlreadyInCart
+    }
+    
+    private func insertOrRemoveTestPackageFromDatabase(at indexPath: IndexPath)
+    {
+        guard let dataModel = filteredTestPackagesDataModel, indexPath.row < dataModel.count else { return }
+        if !dataModel[indexPath.row].isAlreadyInCart
+        {
+            databaseHelper?.insertIntoTable(tableName: testPackagesTableName, fromDictionary: prepareDictionaryForDatabaseInsertion(from: dataModel[indexPath.row]))
+        }
+    }
+    
+    private func prepareDictionaryForDatabaseInsertion(from testPackage: TestPackagesModelElement) -> [String: Any]
+    {
+        var dbInsertionDictionary = [String: Any]()
+        dbInsertionDictionary["sNo"] = testPackage.sNo ?? -1
+        dbInsertionDictionary["itemID"] = testPackage.itemID ?? ""
+        dbInsertionDictionary["itemName"] = testPackage.itemName ?? ""
+        dbInsertionDictionary["type"] = testPackage.type ?? ""
+        dbInsertionDictionary["keyword"] = testPackage.keyword ?? ""
+        dbInsertionDictionary["bestSellers"] = testPackage.bestSellers ?? ""
+        dbInsertionDictionary["testCount"] = testPackage.testCount ?? -1
+        dbInsertionDictionary["includedTests"] = testPackage.includedTests ?? ""
+        dbInsertionDictionary["url"] = testPackage.url ?? ""
+        dbInsertionDictionary["minPrice"] = testPackage.minPrice ?? -1
+        dbInsertionDictionary["labName"] = testPackage.labName ?? ""
+        dbInsertionDictionary["fasting"] = testPackage.fasting ?? -1
+        dbInsertionDictionary["availableAt"] = testPackage.availableAt ?? -1
+        dbInsertionDictionary["popular"] = testPackage.popular ?? ""
+        dbInsertionDictionary["category"] = testPackage.category ?? ""
+        dbInsertionDictionary["objectID"] = testPackage.objectID ?? ""
+
+        if let serialNo = testPackage.sNo
+        {
+            dbInsertionDictionary["sNo"] = serialNo
+        }
+        if let itemId = testPackage.itemID
+        {
+            dbInsertionDictionary["itemID"] = itemId
+        }
+        if let itemName = testPackage.itemName
+        {
+            dbInsertionDictionary["itemName"] = itemName
+        }
+        if let type = testPackage.type
+        {
+            dbInsertionDictionary["type"] = type
+        }
+        if let keyword = testPackage.keyword
+        {
+            dbInsertionDictionary["keyword"] = keyword
+        }
+        if let bestSellers = testPackage.bestSellers
+        {
+            dbInsertionDictionary["bestSellers"] = bestSellers
+        }
+        if let testCount = testPackage.testCount
+        {
+            dbInsertionDictionary["testCount"] = testCount
+        }
+        if let includedTests = testPackage.includedTests
+        {
+            dbInsertionDictionary["includedTests"] = includedTests
+        }
+        if let url = testPackage.url
+        {
+            dbInsertionDictionary["url"] = url
+        }
+        if let minPrice = testPackage.minPrice
+        {
+            dbInsertionDictionary["minPrice"] = minPrice
+        }
+        if let labName = testPackage.labName
+        {
+            dbInsertionDictionary["labName"] = labName
+        }
+        if let fasting = testPackage.fasting
+        {
+            dbInsertionDictionary["fasting"] = fasting
+        }
+        if let availableAt = testPackage.availableAt
+        {
+            dbInsertionDictionary["availableAt"] = availableAt
+        }
+        if let popular = testPackage.popular
+        {
+            dbInsertionDictionary["popular"] = popular
+        }
+        if let category = testPackage.category
+        {
+            dbInsertionDictionary["category"] = category
+        }
+        if let objectID = testPackage.objectID
+        {
+            dbInsertionDictionary["objectID"] = objectID
+        }
+        return dbInsertionDictionary
     }
     
     func fetchTestPackageData(andPerformCompletion completionHandler: ((NetworkError?) -> Void)?)
@@ -97,8 +208,12 @@ extension TestPackageViewModel: TestPackageTableViewCellRepresentable
     func addOrRemoveTestPackageFromCart(at indexPath: IndexPath)
     {
         guard let dataModel = filteredTestPackagesDataModel, indexPath.row < dataModel.count else { return }
-        filteredTestPackagesDataModel?[indexPath.row].isAlreadyInCart                 = !dataModel[indexPath.row].isAlreadyInCart
-        testPackagesDataModel?[dataModel[indexPath.row].actualIndex].isAlreadyInCart  = !dataModel[indexPath.row].isAlreadyInCart
+        
+        insertOrRemoveTestPackageFromDatabase(at: indexPath)        //add it in or remove from db
+        
+        //toggling the cart effect
+        filteredTestPackagesDataModel?[indexPath.row].isAlreadyInCart.toggle()
+        testPackagesDataModel?[dataModel[indexPath.row].actualIndex].isAlreadyInCart.toggle()
     }
     
     func getCartButtonText(at indexPath: IndexPath) -> String
